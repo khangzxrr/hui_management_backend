8ae31af5021644ae483d22b758ae64e4313f5e64
khangzxrr <khangzxrr@gmail.com>
2023-07-10 02:10:26 +0700

    add fund member endpoint

---
+++ src/hui_management_backend.Web/Endpoints/FundEndpoints/FundAddMember.Request.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hui_management_backend.Web.Endpoints.FundEndpoints;
+
+public class FundAddMemberRequest
+{
+  public const string Route = "/funds/{fundId}/members/{memberId}/add";
+
+  [FromRoute]
+  public int fundId { get; set; }
+  [FromRoute]
+  public int memberId { get; set; }
+
+}
---
+++ src/hui_management_backend.Web/Endpoints/FundEndpoints/FundAddMember.cs
@@ -0,0 +1,69 @@
+﻿using Ardalis.ApiEndpoints;
+using hui_management_backend.Core.FundAggregate;
+using hui_management_backend.Core.FundAggregate.Specifications;
+using hui_management_backend.Core.UserAggregate;
+using hui_management_backend.SharedKernel.Interfaces;
+using hui_management_backend.Web.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace hui_management_backend.Web.Endpoints.FundEndpoints;
+
+public class FundAddMember : EndpointBaseAsync
+  .WithRequest<FundAddMemberRequest>
+  .WithActionResult
+{
+
+  private readonly IRepository<Fund> _fundRepository;
+  private readonly IAuthorizeService _authorizeService;
+  private readonly IRepository<User> _userRepository;
+
+  public FundAddMember(IRepository<Fund> fundRepository, IAuthorizeService authorizeService, IRepository<User> userRepository)
+  {
+    _fundRepository = fundRepository;
+    _authorizeService = authorizeService;
+    _userRepository = userRepository;
+  }
+
+  [Authorize]
+  [HttpGet(FundAddMemberRequest.Route)]
+  [SwaggerOperation(
+    Summary = "Fund add member",
+    Description = "Fund add member",
+    OperationId = "Fund.addMember",
+    Tags = new[] { "Fund" }
+    )
+  ]
+  public override async Task<ActionResult> HandleAsync([FromRoute] FundAddMemberRequest request, CancellationToken cancellationToken = default)
+  {
+    var spec = new FundByIdAndOwnerId(request.fundId, _authorizeService.UserId);
+
+    var fund = await _fundRepository.FirstOrDefaultAsync(spec);
+
+    if (fund == null)
+    {
+      return BadRequest("FUND_NOT_FOUND");
+    }
+
+    var user = await _userRepository.GetByIdAsync(request.memberId);
+
+    if (user == null)
+    {
+      return BadRequest("USER_NOT_FOUND");
+    }
+
+    FundMember fundMember = new FundMember
+    {
+      User = user
+    };
+
+    fund.AddMember(fundMember);
+
+    await _fundRepository.UpdateAsync(fund);
+    await _fundRepository.SaveChangesAsync();
+
+
+    return Ok();
+  }
+}

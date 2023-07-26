0e7711aeb957212496360ba806751cef0b7de9d9
khangzxrr <khangzxrr@gmail.com>
2023-07-26 16:48:49 +0700

    azure connection string from environment

--- src/hui_management_backend.Web/Program.cs
+++ src/hui_management_backend.Web/Program.cs
@@ -32,7 +32,7 @@ if (builder.Environment.EnvironmentName == "Development")
   connectionString = builder.Configuration.GetConnectionString("DefaultConnection"); 
 } else
 {
-  connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+  connectionString = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
 }
 
 

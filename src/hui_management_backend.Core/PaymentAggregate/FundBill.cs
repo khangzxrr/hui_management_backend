0b1a0df2947655d65087d67692f9f833c399809e
khangzxrr <khangzxrr@gmail.com>
2023-08-18 03:46:08 +0700

    allow null reference

--- src/hui_management_backend.Core/PaymentAggregate/FundBill.cs
+++ src/hui_management_backend.Core/PaymentAggregate/FundBill.cs
@@ -5,9 +5,9 @@ using hui_management_backend.SharedKernel;
 namespace hui_management_backend.Core.PaymentAggregate;
 public class FundBill : EntityBase
 {
-  public required Fund fromFund { get; set; }
+  public required Fund? fromFund { get; set; }
 
-  public required FundSession fromSession { get; set; }
+  public required FundSession? fromSession { get; set; }
 
-  public required NormalSessionDetail fromSessionDetail { get; set; }
+  public NormalSessionDetail? fromSessionDetail { get; set; }
 }

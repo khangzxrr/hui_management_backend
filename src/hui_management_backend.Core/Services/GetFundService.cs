d995a8c4d322b44d03d6747c369c53badf1a684a
khangzxrr <khangzxrr@gmail.com>
2023-12-30 14:38:42 +0700

    seperate fund service

---
+++ src/hui_management_backend.Core/Services/GetFundService.cs
@@ -0,0 +1,25 @@
+﻿
+using Ardalis.Result;
+using hui_management_backend.Core.FundAggregate;
+using hui_management_backend.Core.FundAggregate.Specifications;
+using hui_management_backend.Core.Interfaces;
+using hui_management_backend.SharedKernel.Interfaces;
+
+namespace hui_management_backend.Core.Services;
+public class GetFundService : IGetFundService
+{
+  private readonly IRepository<Fund> _fundRepository;
+
+  public GetFundService(IRepository<Fund> fundRepository)
+  {
+    _fundRepository = fundRepository;
+  }
+  public async Task<Result<IEnumerable<Fund>>> getFunds(int ownerId, int skip, int take)
+  {
+    var spec = new FundsByOwnerIdSpec(ownerId, skip, take);
+    var funds = await _fundRepository.ListAsync(spec);
+
+
+    return funds;
+  }
+}

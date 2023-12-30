901afe15eb6ee49cee55a9ede5cad700b05949e4
khangzxrr <khangzxrr@gmail.com>
2023-12-31 02:15:37 +0700

    remove unuse import

--- src/hui_management_backend.Core/FundAggregate/Specifications/FundsByOwnerIdSpec.cs
+++ src/hui_management_backend.Core/FundAggregate/Specifications/FundsByOwnerIdSpec.cs
@@ -1,18 +1,25 @@
-﻿using Ardalis.Specification;
+﻿using System.Globalization;
+using System.Text;
+using Ardalis.Specification;
+using Diacritics.Extensions;
 
 namespace hui_management_backend.Core.FundAggregate.Specifications;
 public class FundsByOwnerIdSpec : Specification<Fund>
 {
-  public FundsByOwnerIdSpec(int ownerId, int skip, int take)
+
+  
+
+  public FundsByOwnerIdSpec(int ownerId, int skip, int take, string? searchTerm)
   {
     Query
      .Include(f => f.Owner)
      .Include(f => f.Members)
       .ThenInclude(m => m.subUser)
      .Include(f => f.Sessions)
-      .Where(f => f.Owner.Id == ownerId)
-      .OrderBy(f => f.Id)
-      .Skip(skip)
-      .Take(take);
+     .Where(f => f.Owner.Id == ownerId )
+     .Search(f => f.Name, "%" + searchTerm + "%", searchTerm != null)
+     .OrderBy(f => f.Id)
+     .Skip(skip)
+     .Take(take);
   }
 }

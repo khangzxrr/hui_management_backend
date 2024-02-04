f69e714a824c8f3527fd01d73f0e84e0033231b4
khangzxrr <khangzxrr@gmail.com>
2024-02-04 12:43:52 +0700

    add - add custom bill interface

---
+++ src/hui_management_backend.Core/Interfaces/IAddCustomBillForTodayPaymentService.cs
@@ -0,0 +1,9 @@
+﻿
+using Ardalis.Result;
+using hui_management_backend.Core.PaymentAggregate;
+
+namespace hui_management_backend.Core.Interfaces;
+public interface IAddCustomBillForTodayPaymentService
+{
+  public Task<Result> addCustomBillForTodayPayment(int subuserId, string customBillTypeName, double amount, string description);
+}
